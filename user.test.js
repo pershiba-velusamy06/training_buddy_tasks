83d9a0fddad1c5a022f8358acb4f37e0086337c2
pershiba-velusamy06 <pershiba@elred.io>
2024-03-14 20:21:02 +0530

    refact(user-sign up):removing command lines

--- user.test.js
+++ user.test.js
@@ -1,25 +1,21 @@
 import mongoose from 'mongoose';
-import 'dotenv/config'; // Make sure to have dotenv configured to load environment variables
+import 'dotenv/config'; 
 import request from 'supertest';
 import express from 'express';
 import userRoutes from './src/modules/Users/userRoutes';
 
-// Load environment variables
 const MongoDbString = process.env.MONGODBSTRING;
 
-// Connect to MongoDB
 mongoose.connect(MongoDbString);
 
-// Get the default connection
 const db = mongoose.connection;
 
-// Event listeners for connection events
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Create an express app
+
 const app = express();
 app.use(express.json());
 app.use('/', userRoutes);
@@ -48,7 +44,7 @@ describe('User Routes', () => {
     const invalidUserData = {
       firstname: 'John',
       lastname: 'Doe',
-      phoneNumber: '+911234', // Invalid phone number
+      phoneNumber: '+911234', 
       email: 'john@example.com'
     };
 
@@ -65,8 +61,7 @@ describe('User Routes', () => {
   }, 60000);
 
   it('should return error response for existing email', async () => {
-    const existingEmail = 'existing@example.com'; // Assuming this email already exists in the database
-
+   
     const response = await request(app)
       .post('/userSignUp')
       .send({
@@ -81,8 +76,5 @@ describe('User Routes', () => {
     expect(response.body).toHaveProperty('isAuth', false);
     expect(response.body).toHaveProperty('errorCode', -1);
     expect(response.body).toHaveProperty('message', 'This email is already in use with another phone number. Please enter the correct phone and email combination.');
-    // expect(response.body).toHaveProperty('errors');
-  //  expect(response.body.errors.length).toBeGreaterThan(0);
-    //expect(response.body.errors[0].message).toMatch(/This email is already in use with another phone number. Please enter the correct phone and email combination./); // Check for error message indicating existing email
   }, 60000);
 });

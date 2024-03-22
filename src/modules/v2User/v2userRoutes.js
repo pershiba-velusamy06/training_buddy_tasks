5ce22909e539355568f03dbf8bd6816b80d7d68d
pershiba-velusamy06 <pershiba@elred.io>
2024-03-22 09:25:40 +0530

    feat(otp validation):adding otp validation

--- src/modules/v2User/v2userRoutes.js
+++ src/modules/v2User/v2userRoutes.js
@@ -3,9 +3,9 @@ import express from 'express';
 ;
 import { userHelpers } from '../routeConstants.js';
 import { userValidation } from '../../middleware/userValidationMiddleware.js';
-//import { userValidation } from './userSignUp/userSignUpDataChecker.js';
 import {userSignUp} from './signup/userSignUpController.js'
 import { verifyOtp } from './validateotp/verifyOtpController.js';
+import { otpValidation } from '../../middleware/otpValidationMiddleware.js';
 
 const v2userRoutes = express.Router();
 
@@ -13,7 +13,7 @@ v2userRoutes.post(userHelpers.userSignUp, userValidation, async (req, res) => {
 
   return await userSignUp(req, res);
 });
-v2userRoutes.post(userHelpers.verifyOtp, async (req, res) => {
+v2userRoutes.post(userHelpers.verifyOtp,otpValidation, async (req, res) => {
 
   return await verifyOtp(req, res);
 });
